210149b6865bd0335a6f6f93399554f87299e253
sam650095 <81734861+sam650095@users.noreply.github.com>
2022-05-29 16:21:16 +0800

    Add files via upload

--- search.php
+++ search.php
@@ -1,32 +1,26 @@
-<?php 
-    $colname_rs = $_POST["query"];
-    $result = explode(" ",$_POST["query"]);
-    $link = mysqli_connect("localhost", "root");
+<?php
+$colname_rs = $_POST["query"];
+$result = explode(" ", $_POST["query"]);
+$link = mysqli_connect("localhost", "root");
 mysqli_query($link, "SET NAMES 'UTF8'");
 
-    mysqli_select_db($link, "sa");
-    $query_rs = "";
-    for($i=0;$i<count($result);$i++){
-        if($i==0) 
-            $query_rs = "SELECT * FROM book_info WHERE book_name LIKE '%$result[0]%' or book_author LIKE '%$result[0]%' or public LIKE '%$result[0]%'";
-        else 
-            $query_rs .= " UNION SELECT * FROM book_info WHERE book_name LIKE '$result[$i]' ";
-    }
+mysqli_select_db($link, "sa");
+$query_rs = "SELECT * FROM book_info WHERE book_name LIKE '%$result[0]%' or book_author LIKE '%$result[0]%' or public LIKE '%$result[0]%' group by book_name";
 
-    $query_rs .= " ORDER BY book_name DESC"; 
-    $rs = mysqli_query($link, $query_rs);
-    $row_rs = mysqli_fetch_assoc($rs);
-    $totalRows_rs = mysqli_num_rows($rs);
+
+$query_rs .= " ORDER BY book_name DESC";
+$rs = mysqli_query($link, $query_rs);
+$row_rs = mysqli_fetch_assoc($rs);
+$totalRows_rs = mysqli_num_rows($rs);
 ?>
 
 <style>
-    .book_jpg_style123{
-        float:left;
-        margin-right:40px;
+    .book_jpg_style123 {
+        float: left;
+        margin-right: 40px;
         clear: both;
         height: 400px;
     }
-
 </style>
 
 <html>
@@ -49,14 +43,14 @@ mysqli_query($link, "SET NAMES 'UTF8'");
 
                 <!-- Header -->
                 <header id="header">
-                    
+
                     <section id="search" class="alt">
-                    <form method="post" action="search.php">
+                        <form method="post" action="search.php">
                             <input type="text" name="query" id="query" placeholder="輸入關鍵字" />
                         </form>
-                         <a href="index.php" class="logo"><strong>首頁</strong></a>
+                        <a href="index.php" class="logo"><strong>首頁</strong></a>
                     </section>
-                   
+
                 </header>
 
                 <!-- Content -->
@@ -65,23 +59,24 @@ mysqli_query($link, "SET NAMES 'UTF8'");
                     <!-- Content -->
                     <h2 id="content">搜尋結果</h2>
                     <hr class="major" />
-                    
+
                     <!--搜尋書籍關鍵字結果-->
                     <p align="center"><B>關鍵詞搜索結果如下：</B></p>
-                    
-                    <?php if($totalRows_rs>0) do { ?>
+
+                    <?php if ($totalRows_rs > 0) do { ?>
                         <div class="book_jpg_style123">
-                            <a href="書籍內容.php?book=<?php echo $row_rs['book_name'] ?>"><img class="book_image" src="images/<?php echo $row_rs['book_image'];?>" /></a>
+                            <a href="書籍內容.php?book_id=<?php echo $row_rs['book_id'] ?>">
+                            <img src="images/<?php echo $row_rs['book_image']; ?>" /></a>
                         </div>
-                    <p>書名 : <?php echo $row_rs["book_name"]; ?><br></p>
-                    <p>作者 : <?php echo $row_rs["book_author"]; ?><br></p>
-                    <p>出版社 : <?php echo $row_rs["public"]; ?><br></p>
-                    <p>出版日期 : <?php echo $row_rs["public_date"]; ?><br></p>
-                    <p>類別 : <?php echo $row_rs["book_category"]; ?><br></p>
-                    <p>簡介 : <?php echo $row_rs["book_introduction"]; ?><br></p>
-                    <br><br><br><br>
+                        <p>書名 : <?php echo $row_rs["book_name"]; ?><br></p>
+                        <p>作者 : <?php echo $row_rs["book_author"]; ?><br></p>
+                        <p>出版社 : <?php echo $row_rs["public"]; ?><br></p>
+                        <p>出版日期 : <?php echo $row_rs["public_date"]; ?><br></p>
+                        <p>類別 : <?php echo $row_rs["book_category"]; ?><br></p>
+                        <p>簡介 : <?php echo $row_rs["book_introduction"]; ?><br></p>
+                        <br><br><br><br>
                     <?php } while ($row_rs = mysqli_fetch_assoc($rs)); ?>
-                    
+
 
                 </section>
 
