201d64b68f939c455448afec2960c81b2985afca
sam650095 <81734861+sam650095@users.noreply.github.com>
2022-05-30 14:50:44 +0800

    Add files via upload

--- search.php
+++ search.php
@@ -1,17 +1,16 @@
 <?php
 $colname_rs = $_POST["query"];
-$result = explode(" ", $_POST["query"]);
 $link = mysqli_connect("localhost", "root");
 mysqli_query($link, "SET NAMES 'UTF8'");
 
 mysqli_select_db($link, "sa");
-$query_rs = "SELECT * FROM book_info WHERE book_name LIKE '%$result[0]%' or book_author LIKE '%$result[0]%' or public LIKE '%$result[0]%' group by book_name";
+
+$query_rs = "SELECT * FROM book_info WHERE book_name LIKE '%$colname_rs%' or book_author LIKE '%$colname_rs%' or public LIKE '%$colname_rs%' group by book_name";
 
 
 $query_rs .= " ORDER BY book_name DESC";
 $rs = mysqli_query($link, $query_rs);
-$row_rs = mysqli_fetch_assoc($rs);
-$totalRows_rs = mysqli_num_rows($rs);
+
 ?>
 
 
@@ -38,7 +37,7 @@ $totalRows_rs = mysqli_num_rows($rs);
 
                     <section id="search" class="alt">
                         <form method="post" action="search.php">
-                            <input type="text" name="query" id="query" placeholder="輸入關鍵字" />
+                            <input type="text" name="query" placeholder="輸入關鍵字" />
                         </form>
                         <a href="index.php" class="logo"><strong>首頁</strong></a>
                     </section>
@@ -55,7 +54,8 @@ $totalRows_rs = mysqli_num_rows($rs);
                     <!--搜尋書籍關鍵字結果-->
                     <p align="center"><B>關鍵詞搜索結果如下：</B></p>
 
-                    <?php while ($row_rs = mysqli_fetch_assoc($rs)) { ?>
+                    <?php while ($row_rs = mysqli_fetch_assoc($rs)) {
+                        ?>
 
                         <div class="box box_action">
                             <div class="book_jpg_style123">
