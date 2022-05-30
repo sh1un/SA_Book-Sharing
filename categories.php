206d60d4d2d551d201d3fb86bf4b6a65a0052613
sam650095 <81734861+sam650095@users.noreply.github.com>
2022-05-30 23:56:17 +0800

    Add files via upload

--- categories.php
+++ categories.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-$name = $_SESSION['name'];
+$account = $_SESSION['account'];
 $category = $_GET['cat'];
 $link = mysqli_connect("localhost", "root");
 mysqli_query($link, "SET NAMES 'UTF8'");
@@ -75,7 +75,11 @@ $rs = mysqli_query($link, $sql);
 									上架時間 : <?php echo $rslt['up_date']; ?><br>
 									<?php echo $rslt['book_introduction']; ?></p>
 									<ul class="actions">
-										<li><a href="書籍內容.php?book_id=<?php echo $rslt['book_id'] ?>" class="button">立即借閱</a></li>
+										<li><?php if ($rslt["book_owner"] == $account) { ?><a href="下架書籍.php?book_id=$book_id" class="button big">下架</a>
+                                    <?php } else if ($rslt["book_user"] == $account) { ?><a href='return.php?book_id=<?php echo $rslt[0]; ?>' class="button big">還書</a><?php } 
+									else { ?>
+                                        <a href="borr.php?br=b" class="button big">立即借閱</a><?php } ?>
+                                </li></li>
 									</ul>
 								</div>
 							</article>
@@ -88,58 +92,6 @@ $rs = mysqli_query($link, $sql);
 					</div>
 				</section>
 
-				<!-- Section 
-				<section>
-					<header class="major">
-						<h2>哲學類</h2>
-					</header>
-					<div class="features">
-						<article>
-							<span><img src="images/沉思錄.jpg" alt="" /></span>
-							<div class="content">
-								<h3>沉思錄</h3>
-								<p>作者：馬可·奧理略</p>
-								<p>書籍簡介：影響西方文化兩千年的傳世經典，最完整台灣新譯一代賢帝的立身處世之道，欲成大器者必讀之書<br></p>
-								<ul class="actions">
-									<li><a href="#" class="button">立即借閱</a></li>
-								</ul>
-							</div>
-						</article>
-						<article>
-							<span><img src="images/哲學與人生.jpg" alt="" /></span>
-							<div class="content">
-								<h3>哲學與人生</h3>
-								<p>作者：傅佩榮</p>
-								<p>書籍簡介：「哲學」做為一門學問，原來只是一種生活態度，就是保持好奇的天性，探詢一切事物的真相。這種態度稱為「愛智」。<br></p>
-								<ul class="actions">
-									<li><a href="#" class="button">立即借閱</a></li>
-								</ul>
-							</div>
-						</article>
-						<article>
-							<span><img src="images/廁所裡的哲學課.jpg" alt="" /></span>
-							<div class="content">
-								<h3>廁所裡的哲學課</h3>
-								<p>作者：亞當弗萊徹.盧卡斯‧NP‧艾格.康拉德柯列弗</p>
-								<p>書籍簡介：平均來說，我們每天會花14分鐘蹲馬桶，既然如此，何不善用蹲馬桶的寶貴時間，學點真正有用的東西？<br></p>
-								<ul class="actions">
-									<li><a href="#" class="button">立即借閱</a></li>
-								</ul>
-							</div>
-						</article>
-						<article>
-							<span><img src="images/西方哲學與人生.jpg" alt="" /></span>
-							<div class="content">
-								<h3>西方哲學與人生</h3>
-								<p>作者：亞當弗萊徹</p>
-								<p>書籍簡介：哲學就是人生經濟學，用最少時間，達成最大效果。<br></p>
-								<ul class="actions">
-									<li><a href="#" class="button">立即借閱</a></li>
-								</ul>
-							</div>
-						</article>
-					</div>
-				</section>-->
 
 				
 			</div>
