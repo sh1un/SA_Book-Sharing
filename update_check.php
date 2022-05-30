7eb98188daa9fbee18704e9c91d613519bd9df35
Ariel1206 <ariel70205@gmail.com>
2022-05-30 22:18:58 +0800

    debug

--- update_check.php
+++ update_check.php
@@ -18,7 +18,7 @@ echo $udgender.$udarea.$udaddress.$account.$udcon;
 
 $sql = "UPDATE `account` SET `name`='$udname',`password`='$udpassword',`email`='$udemail',`birth`='$udbirth'
 ,`area`='$udarea',`address`='$udaddress',`gender`='$udgender',`con`='$udcon' WHERE account = '$account'";
-$sql2 = "UPDATE `book_info` SET `connection_method` = '$udcon' WHERE book_owner = '$account'";
+$sql2 = "UPDATE `book_info` SET `connection_method` = '$udcon' , `book_owner=` = '$udname' WHERE book_owner = '$account'";
 
 if (mysqli_query($link, $sql)) {
     header('location:member.php?資料已修改');
