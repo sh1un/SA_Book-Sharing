b6ebca1e40a13b75b18f002725fc8f24c4f7414f
Alvin59567 <104577241+Alvin59567@users.noreply.github.com>
2022-06-04 18:31:32 +0800

    修正編輯書籍點進去永遠都是最後一本

--- 已上架書籍.php
+++ 已上架書籍.php
@@ -98,7 +98,7 @@ if (isset($_GET['sorf'])) {
 
                                 <ul class="actions">
                                     <li><a href="下架書籍.php?book_id=<?php echo $rslt['book_id'] ?>" class="button">下架</a></li>
-                                    <li><input type="submit" class="input_btn" value="編輯資訊"></li>
+                                    <li><a href="編輯書籍.php?book_id=<?php echo $rslt['book_id'] ?>" class="button">編輯書籍</a></li>
                                     
 
                                 </ul>
--- 編輯書籍.php
+++ 編輯書籍.php
@@ -3,7 +3,7 @@ session_start();
 if (isset($_SESSION['name'])) {
     $name = $_SESSION['name'];
     $account = $_SESSION['account'];
-    $book_id = $_POST['book_id'];
+    $book_id = $_GET['book_id'];
     $link = mysqli_connect("localhost", "root");
 
     mysqli_query($link, "SET NAMES 'UTF8'");
@@ -53,33 +53,29 @@ if (isset($_SESSION['name'])) {
                     <form action="編輯書籍_check.php" method="POST">
                     <div class="posts">
                         <?php
-                        while ($rslt =  mysqli_fetch_assoc($rs)) {
-                            $bookuser_account =  $rslt['book_user'];
-                            $sql2 = "select * from account where account = '$bookuser_account'";
-                            $reslt = mysqli_query($link, $sql2); //持書者的名字
-                            $rslt2 =  mysqli_fetch_assoc($reslt);
+                            $rs =  mysqli_fetch_assoc($rs);
                         ?>
                             <article>
                                 <div class="img_box">
                                 更換圖片：<input type="file" name="udbook-image" id="book-image" accept=".jpg, .png, .img, .jpeg" value=""/>
-                                <input type="hidden" value=<?php echo $_POST['book_image']; ?> name="book-image">
-                                    <img class="img_item" src='images/<?php echo $_POST['book_image']; ?>' alt="" /></div>
-                                書名：<h3><input type="text" name="udbook-name" id="book-name" value="<?php echo $_POST['book_name'] ?>"/></h3>
-                                編號 : <input type="hidden" value=<?php echo $_POST['book_id'] ?> name="book_id">
-                                <?php echo $_POST['book_id'] ?>
-                                <p><?php if ($rslt['book_user'] == "none") {
+                                <input type="hidden" value=<?php echo $rs['book_image']; ?> name="book-image">
+                                    <img class="img_item" src='images/<?php echo $rs['book_image']; ?>' alt="" /></div>
+                                書名：<h3><input type="text" name="udbook-name" id="book-name" value="<?php echo $rs['book_name'] ?>"/></h3>
+                                編號 : <input type="hidden" value=<?php echo $rs['book_id'] ?> name="book_id">
+                                <?php echo $rs['book_id'] ?>
+                                <p><?php if ($rs['book_user'] == "none") {
                                     ?><input type="hidden" value="none" name="book_user">
                                     租借情況：none <br> 租借人：none<?php
-                                    } else { ?><input type="hidden" value="<?php echo $rslt2['name']?>" name="book_user">
+                                    } else { ?><input type="hidden" value="<?php echo $rs['name']?>" name="book_user">
                                     租借情況：租借中 <br> 租借人 : <?php
-                                            echo $rslt2['name'];}?>
+                                            echo $rs['name'];}?>
                                 <br>捐借人：<input type="hidden" value=<?php echo $name ?> name="book_owner">
                                 <?php echo $name ?>
-                                <br>上架時間：<input type="hidden" value=<?php echo $rslt['up_date'] ?> name="up_date">
-                                <?php echo $_POST['up_date'] ?>
-                                <br>作者：<input type="text" name="udbook-author" id="book-author"  value="<?php echo $_POST['book_author'] ?>" required>
-                                <br>出版社：<input type="text" name="udpublic" id="public" value="<?php echo $_POST['public'] ?>"/>
-                                <br>出版日期：<input type="date" name="udpublic-date" id="public-date" value="<?php echo $_POST['public_date'] ?>"/>
+                                <br>上架時間：<input type="hidden" value=<?php echo $rs['up_date'] ?> name="up_date">
+                                <?php echo $rs['up_date'] ?>
+                                <br>作者：<input type="text" name="udbook-author" id="book-author"  value="<?php echo $rs['book_author'] ?>" required>
+                                <br>出版社：<input type="text" name="udpublic" id="public" value="<?php echo $rs['public'] ?>"/>
+                                <br>出版日期：<input type="date" name="udpublic-date" id="public-date" value="<?php echo $rs['public_date'] ?>"/>
                                 <br>類別：<select name="udbook-category" id="book-category" required>
                                     <option selected="selected" disabled>請重新選擇類別</option>
                                     <option value="總類">000 總類</option>
@@ -101,7 +97,6 @@ if (isset($_SESSION['name'])) {
                                 <input type="submit" class="input_btn" value="確認修改"><br>
                                 </div>
                             </article>
-                        <?php } ?>
 
 
                     </div>
