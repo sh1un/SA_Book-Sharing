d0f19a5804aed0a093795433476245fb1e0eef2e
Shiun0413 <a0923183408@gmail.com>
2022-06-05 09:30:57 +0800

    feat:  新增 根據order_check更改order_status功能

--- order_check.php
+++ order_check.php
@@ -29,14 +29,34 @@
         <input type='hidden' name='order_id' value="<?php echo $order_id; ?>">
     </form>
 <?php
+// 點擊使Order_check + 1
     $fetch_order_check="SELECT order_check FROM orderlist WHERE order_id='$order_id' ";
     $rs=mysqli_query($link, $fetch_order_check);
     $record=mysqli_fetch_assoc($rs);
     $record2=$record['order_check'];
     $sql="UPDATE orderlist SET order_check = $record2 + 1 WHERE order_id='$order_id'";
     mysqli_query($link, $sql);
-    header("Location: order.php");
+?>
+
+<?php
+// 根據order_check 更改 order_status
+    if($record['order_check']+1 == 2){
+        $sql2="UPDATE orderlist SET order_status = '待還書' WHERE order_id='$order_id'";
+        mysqli_query($link, $sql2);
+        
+    }
+    elseif($record['order_check']+1 == 4){
+        $sql3="UPDATE orderlist SET order_status = '待評價' WHERE order_id='$order_id'";
+        mysqli_query($link, $sql3);
+    }
+    elseif($record['order_check']+1 == 5){
+        $sql4="UPDATE orderlist SET order_status = '已完成' WHERE order_id='$order_id'";
+        mysqli_query($link, $sql4);
+    }
+?>
+<?php
     mysqli_close($link);
+    header("location:order.php?");
 ?>
 </body>
 </html>
