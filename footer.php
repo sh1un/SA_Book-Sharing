2d5a5bf85a157d83d703abad6a97886fd6c3d815
Alvin59567 <104577241+Alvin59567@users.noreply.github.com>
2022-06-11 21:43:03 +0800

    新增footer.php 將頁尾獨立出來，方便其他檔案include

---
+++ footer.php
@@ -0,0 +1,24 @@
+<footer id="footer" style="background-color: #F5FFFA">
+                <hr>
+                <div class="row">
+                    
+                    <div class=col-12 style="margin-left: 7%;margin-top: 2%">
+                        <h2 style="color: #B22222">書籍共享平台</h2>
+                    </div>
+                    <div class="col-4" style="margin-left: 7%;">
+                    <ul class="contact">
+                        <li class="icon solid fa-envelope"><a href="#">409401289@mail.fju.edu.tw</a></li>
+                        <li class="icon solid fa-phone">(000) 000-0000</li>
+                    </ul>
+                    </div>
+                    <div class="col-4">
+                        <ul class="contact">
+                            <li class="icon solid fa-home">1234 Somewhere Road #8254<br> Nashville, TN 00000-0000</li>
+                        </ul>
+                    </div>
+                    <div class="col-3" style="margin-top: 3%;">
+                        <h3>輔仁大學SA第一組</h3>
+                    </div>
+                </div>
+                <p class="copyright" style="margin-left: 50%;margin-bottom:10px">© Untitled. All rights reserved. Demo Images: <a href="https://unsplash.com">Unsplash</a>. Design: <a href="https://html5up.net">HTML5 UP</a>.</p>
+            </footer>
