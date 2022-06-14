addcc4d9a3c35db759421bb5b63780d2e46edba6
Shiun0413 <a0923183408@gmail.com>
2022-06-14 23:40:30 +0800

    fix: fixed #101

--- categories.php
+++ categories.php
@@ -1,4 +1,5 @@
 <?php
+$account = $_SESSION['account'];
 $category = $_GET['cat'];
 $link = mysqli_connect("localhost", "root");
 mysqli_query($link, "SET NAMES 'UTF8'");
@@ -9,7 +10,6 @@ $text = '排序方式';
 if (!(isset($_GET['way']))) {
     $query_sql = "select * from book_info where book_category = '$category' group by ISBN";
 }
-
 
 if (isset($_GET['way'])) {
 
@@ -34,6 +34,7 @@ if (isset($_GET['way'])) {
 
     
 }
+
 
 $query_rs = mysqli_query($link, $query_sql);
 
@@ -66,19 +67,19 @@ $query_rs = mysqli_query($link, $query_sql);
 				<header id="header">
 					<a href="index.php" class="logo"><strong>首頁</strong></a>
 					<?php
-
-					if (isset($_SESSION['name'])) {
-						$name = $_SESSION['name'];
-						$account = $_SESSION['account'];
-						echo "<ul class='icons'>
+                        
+                        if (isset($_SESSION['name'])) {
+                            $name = $_SESSION['name'];
+                            $account = $_SESSION['account'];
+                            echo "<ul class='icons'>
                                 <li><p>$name ，歡迎光臨 <a href='logout.php' class='button primary small'>登出</span></a></p></li>
                                 </ul>";
-					} else {
-						echo "<ul class='icons'>
+                        } else {
+                            echo "<ul class='icons'>
                                 <li><a href='login.php' class='button primary small'>登入</span></a></li>
                                 </ul>";
-					}
-					?>
+                        }
+                        ?>
 
 				</header>
 
@@ -87,37 +88,36 @@ $query_rs = mysqli_query($link, $query_sql);
 				<!-- 瀏覽Section  -->
 				<section>
 					<header class="major">
-						<h2><?php echo $category ?></h2>
+						<h2><?php echo $category?></h2>
 					</header>
-
+					<form method="get" action="">
 					<h2 id="content">分類結果</h2>
 					<select name="way" onchange="location=this.value;" required>
-						<option selected="selected" disabled><?php echo $text; ?>
-						<option value="categories.php?cat=&value=<?php echo $category?>way=最新">最新
-						<option value="categories.php?cat=&value=<?php echo $category?>way=最舊">最舊
-						<option value="categories.php?cat=&value=<?php echo $category?>way=最多愛心">最多愛心
-						<option value="categories.php?cat=&value=<?php echo $category?>way=名稱">名稱
+						<option selected="selected" disabled><?php echo $text; ?></option>
+						<option value="categories.php?cat=<?php echo $category?>&way=最新">最新</option>
+						<option value="categories.php?cat=<?php echo $category?>&way=最舊">最舊</option>
+						<option value="categories.php?cat=<?php echo $category?>&way=最多愛心">最多愛心</option>
+						<option value="categories.php?cat=<?php echo $category?>&way=名稱">名稱</option>
 					</select>
-
-					<input type="hidden" name="cat" value=<?php echo $category; ?>
-
+					</form>
+					 
+					
 					<div class="featuresforbrowse">
 						<?php
 						while ($rslt =  mysqli_fetch_assoc($query_rs)) {
 						?>
 							<article>
-								<div style="width:30%; height:30%"><img style="width:100%; height:200%" src="images/<?php echo $rslt['book_image']; ?>" alt="" /></div>
+								<div style="width:30%; height:30%"><img style="width:100%; height:200%"src="images/<?php echo $rslt['book_image']; ?>" alt="" /></div>
 								<div class="content">
 									<h3><?php echo $rslt['book_name']; ?></h3>
 									<p>作者 : <?php echo $rslt['book_author']; ?><br>
-										出版社 : <?php echo $rslt['public']; ?><br>
-										類別：<?php echo $rslt['book_category']; ?><br>
-										上架時間 : <?php echo $rslt['up_date']; ?><br>
-										<?php echo $rslt['book_introduction']; ?></p>
+									出版社 : <?php echo $rslt['public']; ?><br>
+									類別：<?php echo $rslt['book_category']; ?><br>
+									上架時間 : <?php echo $rslt['up_date']; ?><br>
+									<?php echo $rslt['book_introduction']; ?></p>
 									<ul class="actions">
 										<li><a href="書籍內容.php?ISBN=<?php echo $rslt['ISBN'] ?>" class="button big">書籍資訊</a>
-										</li>
-										</li>
+                                </li></li>
 									</ul>
 								</div>
 							</article>
@@ -131,7 +131,7 @@ $query_rs = mysqli_query($link, $query_sql);
 				</section>
 
 
-
+				
 			</div>
 		</div>
 
